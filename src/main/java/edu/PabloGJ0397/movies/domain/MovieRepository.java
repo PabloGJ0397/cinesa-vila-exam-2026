98927978f28df19eb522bb3db8e22e1f00e66b88
GJP <garciajuarezpablo97@gmail.com>
2026-06-10 16:44:27 +0200

    feat(domain): Add getMovies() to MovieRepository

--- src/main/java/edu/PabloGJ0397/movies/domain/MovieRepository.java
+++ src/main/java/edu/PabloGJ0397/movies/domain/MovieRepository.java
@@ -1,5 +1,8 @@
 package edu.PabloGJ0397.movies.domain;
 
+import java.util.ArrayList;
+
 public interface MovieRepository {
     public void save(Movie movie);
+    public ArrayList<Movie> getMovies();
 }
