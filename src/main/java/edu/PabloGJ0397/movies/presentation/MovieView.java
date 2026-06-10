57434694ef4c83e4aa1bd7c35f8e0314ec2be25e
GJP <garciajuarezpablo97@gmail.com>
2026-06-10 16:45:30 +0200

    feat(presentation): add method GetMovies to MovieView

--- src/main/java/edu/PabloGJ0397/movies/presentation/MovieView.java
+++ src/main/java/edu/PabloGJ0397/movies/presentation/MovieView.java
@@ -2,13 +2,21 @@ package edu.PabloGJ0397.movies.presentation;
 
 import edu.PabloGJ0397.movies.data.MovieDataRepository;
 import edu.PabloGJ0397.movies.data.MovieMemLocalDataSource;
+import edu.PabloGJ0397.movies.domain.GetMoviesUseCase;
 import edu.PabloGJ0397.movies.domain.Movie;
 import edu.PabloGJ0397.movies.domain.SaveMovieUseCase;
+
+import java.util.ArrayList;
 
 public class MovieView {
     public static void save(Movie movie){
         SaveMovieUseCase saveMovieUseCase = new SaveMovieUseCase(new MovieDataRepository(MovieMemLocalDataSource.getInstance()));
         saveMovieUseCase.execute(movie);
 
+    }
+    public static void getMovies(){
+        GetMoviesUseCase getMoviesUseCase = new GetMoviesUseCase(new MovieDataRepository(MovieMemLocalDataSource.getInstance()));
+        ArrayList<Movie> movies = getMoviesUseCase.execute();
+        System.out.println(movies);
     }
 }
